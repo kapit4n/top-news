43218d74cead546505466c0a02f403aa51f84a26
larce <luis.arce22@gmail.com>
2017-04-08 22:24:19 -0400

    Adding news component

---
+++ src/app/components/news/news.spec.js
@@ -0,0 +1,7 @@
+import News from './news.vue';
+
+describe('News', () => {
+  it('should be a news', () => {
+    expect(News.name).toEqual('news');
+  });
+});
---
+++ src/app/components/news/news.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ text }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'news',
+  data() {
+    return {
+      text: 'My new component'
+    };
+  }
+};
+</script>
