7ca90bca551243dbc7955e325e983aa73b90d13a
lartl <luis.arce22@gmail.com>
2018-10-10 07:51:58 -0400

    Changed image position in news component

--- src/app/components/news/news.vue
+++ src/app/components/news/news.vue
@@ -1,10 +1,10 @@
 <template>
 <div style="width: 250px; display: inline-block;  border-style: dotted;" v-if="newsData.title !== 'noTitle' && newsData.imgUrl !== 'noImage'">
-  <h3 style="height: 50px; font-size: 20px; font-weight: bold;"> <a v-bind:href="newsData.link" style="color: black;">{{ newsData.title }}</a></h3>
-  <p style="height: 100px;">{{ newsData.body }}</p>
 	<div v-if="newsData.imgUrl !== 'noImage'">
   	<img v-bind:src="newsData.imgUrl" style="height: 130px;">
 	</div >
+  <h3 style="font-size: 20px; font-weight: bold;"> <a v-bind:href="newsData.link" style="color: black;">{{ newsData.title }}</a></h3>
+  <p style="height: 100px;">{{ newsData.body }}</p>
 </div>
 </template>
 
